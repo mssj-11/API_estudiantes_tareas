38942d8d75ee358067b623b9e1a37a1def719609
mssj-11 <michael11.salgado10@gmail.com>
2023-09-19 17:53:36 -0600

    METHOD POST - Students

--- index.php
+++ index.php
@@ -14,7 +14,31 @@ Flight::route('GET /students', function(){
 });
 
 //  POST
-Flight::route('POST /students', function(){});
+Flight::route('POST /students', function(){
+    $full_name = (Flight::request()->data->full_name);
+    $age = (Flight::request()->data->age);
+    $enrollment = (Flight::request()->data->enrollment);
+    $address = (Flight::request()->data->address);
+    $tel = (Flight::request()->data->tel);
+    $email = (Flight::request()->data->email);
+    $gender = (Flight::request()->data->gender);
+    $comments = (Flight::request()->data->comments);
+
+    $sql = 'INSERT INTO students (full_name, age, enrollment, address, tel, email, gender, comments) VALUES(?,?,?,?,?,?,?,?)';
+    $sentence = Flight::db()->prepare($sql);
+
+    $sentence->bindParam(1, $full_name);
+    $sentence->bindParam(2, $age);
+    $sentence->bindParam(3, $enrollment);
+    $sentence->bindParam(4, $address);
+    $sentence->bindParam(5, $tel);
+    $sentence->bindParam(6, $email);
+    $sentence->bindParam(7, $gender);
+    $sentence->bindParam(8, $comments);
+    $sentence->execute();
+
+    Flight::jsonp(['Added Student !!']);
+});
 
 //  PUT
 Flight::route('PUT /students', function(){});
