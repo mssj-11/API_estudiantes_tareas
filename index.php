65f6f27c00d13f659b276f3bf5149f7ed6060706
mssj-11 <michael11.salgado10@gmail.com>
2023-09-19 17:40:14 -0600

    METHOD GET - Students

--- index.php
+++ index.php
@@ -3,6 +3,28 @@ require 'flight/Flight.php';
 //  Database Connection
 Flight::register('db', 'PDO', array('mysql:host=localhost;dbname=api_estudiantes_tareas', 'root', ''));
 
+/*   METHODS  */
+//  GET
+Flight::route('GET /students', function(){
+    $sql = 'SELECT * FROM students';
+    $sentence = Flight::db()->prepare($sql);
+    $sentence->execute();
+    $students = $sentence->fetchAll(PDO::FETCH_ASSOC);
+    Flight::json($students);
+});
+
+//  POST
+Flight::route('POST /students', function(){});
+
+//  PUT
+Flight::route('PUT /students', function(){});
+
+//  DELETE
+Flight::route('DELETE /students', function(){});
+
+//  SEARCH BY ID
+Flight::route('GET /students/@id', function(){});
+
 
 Flight::start();
 ?>
